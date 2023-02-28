137c5b7e0f9cefa795719e247ddb42521b265ee4
Enzo Prina <enzoprina9@gmail.com>
2023-02-28 14:25:24 -0300

    card negra y todos los proyectos

--- src/components/Project.js
+++ src/components/Project.js
@@ -13,15 +13,10 @@ import Planificador from "../../public/ProyectosFotos/PlanificadorDeGastos.jpg";
 import Cripto from "../../public/ProyectosFotos/Criptomonedas.jpg";
 import Noticias from "../../public/ProyectosFotos/Noticias.jpg";
 import Notas from "../../public/ProyectosFotos/AppNotas.jpg";
-import Blog from "../../public/ProyectosFotos/BlogdeCafe.jpg";
-/* import CardI from "./proyectos/CardI"; */
-/* import CardII from "./proyectos/CardII"; */
-/* import CardIII from "./proyectos/CardIII";
-import CardIV from "./proyectos/CardIV";
-import CardV from "./proyectos/CardV";
-import CardVI from "./proyectos/CardVI";
-import CardVII from "./proyectos/CardVII";
-import CardVIII from "./proyectos/CardVIII"; */
+import Blog from '../../public/ProyectosFotos/BlogdeCafe.jpg'
+import Poke from '../../public/ProyectosFotos/Pokedex_Mesa de trabajo 1-min.jpg'
+import Link from "next/link";
+
 
 const Project = () => {
   return (
@@ -62,9 +57,11 @@ const Project = () => {
                 </CardContent>
               </CardActionArea >
               <CardActions className={styles.bgBoton}>
+              <Link href="https://dashboard-client-wheat.vercel.app/" legacyBehavior>
                 <Button className={styles.button} size="small" color="primary">
                   Ver proyecto
                 </Button>
+                </Link>
               </CardActions>
             </Card>
           </div>
@@ -93,9 +90,11 @@ const Project = () => {
                 </CardContent>
               </CardActionArea>
               <CardActions className={styles.bgBoton} >
+              <Link href="https://yardsaleshop.netlify.app/" legacyBehavior>
                 <Button className={styles.button} size="small" color="primary">
                   Ver proyecto
                 </Button>
+                </Link>
               </CardActions>
             </Card>
           </div>
@@ -124,9 +123,11 @@ const Project = () => {
                 </CardContent>
               </CardActionArea>
               <CardActions className={styles.bgBoton} >
+              <Link href="https://dashboard-nextjs-gamma.vercel.app/" legacyBehavior>
                 <Button className={styles.button} size="small" color="primary">
                   Ver proyecto
                 </Button>
+                </Link>
               </CardActions>
             </Card>
           </div>
@@ -154,9 +155,11 @@ const Project = () => {
                 </CardContent>
               </CardActionArea>
               <CardActions className={styles.bgBoton} >
+              <Link href="https://veterinaria-seguimientopacientes.netlify.app/" legacyBehavior>
                 <Button className={styles.button} size="small" color="primary">
                   Ver proyecto
                 </Button>
+                </Link>
               </CardActions>
             </Card>
           </div>
@@ -184,9 +187,11 @@ const Project = () => {
                 </CardContent>
               </CardActionArea>
               <CardActions className={styles.bgBoton} >
+              <Link href="https://control-presup-gasto.netlify.app/" legacyBehavior>
                 <Button className={styles.button} size="small" color="primary">
                   Ver proyecto
                 </Button>
+                </Link>
               </CardActions>
             </Card>
           </div>
@@ -214,9 +219,11 @@ const Project = () => {
                 </CardContent>
               </CardActionArea>
               <CardActions className={styles.bgBoton} >
+              <Link href="https://cotiz-cripto.netlify.app/" legacyBehavior>
                 <Button className={styles.button} size="small" color="primary">
                   Ver proyecto
                 </Button>
+                </Link>
               </CardActions>
             </Card>
           </div>
@@ -244,9 +251,11 @@ const Project = () => {
                 </CardContent>
               </CardActionArea>
               <CardActions className={styles.bgBoton} >
+              <Link href="https://magazine-notice.netlify.app/" legacyBehavior>
                 <Button className={styles.button} size="small" color="primary">
                   Ver proyecto
                 </Button>
+                </Link>
               </CardActions>
             </Card>
           </div>
@@ -274,12 +283,97 @@ const Project = () => {
                 </CardContent>
               </CardActionArea>
               <CardActions className={styles.bgBoton} >
+              <Link href="https://add-new-note-app.netlify.app/" legacyBehavior>
                 <Button className={styles.button} size="small" color="primary">
                   Ver proyecto
                 </Button>
+                </Link>
               </CardActions>
             </Card>
           </div>
+
+          <div className={styles.card}>
+            {" "}
+            <Card sx={{ maxWidth: 400 }}>
+              <CardActionArea className={styles.bgCard}>
+                <div>
+                  <Image
+                    src={Blog}
+                    alt="logo"
+                    width={400}
+                    className={styles.oscurecerImagen}
+                  />
+                </div>
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                     /* color="text.primary" */ 
+                  >
+                    Blog de Cafe
+                  </Typography>
+                  <Typography variant="body3"  /* color="text.secondary" */ >
+                    realizado como practica
+                    <br />{" "}
+                    <span className={styles.resaltado}>
+                      {" "}
+                      HTML5 | CSS3 | JAVASCRIPT VANILLA |
+                    </span>
+                  </Typography>
+                </CardContent>
+              </CardActionArea >
+              <CardActions className={styles.bgBoton}>
+              <Link href="https://tu-blogdecafe.netlify.app/" legacyBehavior>
+                <Button className={styles.button} size="small" color="primary">
+                  Ver proyecto
+                </Button>
+                </Link>
+              </CardActions>
+            </Card>
+          </div>
+
+          <div className={styles.card}>
+            {" "}
+            <Card sx={{ maxWidth: 400 }}>
+              <CardActionArea className={styles.bgCard}>
+                <div>
+                  <Image
+                    src={Poke}
+                    alt="logo"
+                    width={400}
+                    className={styles.oscurecerImagen}
+                  />
+                </div>
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                     /* color="text.primary" */ 
+                  >
+                    PokeDex
+                  </Typography>
+                  <Typography variant="body3"  /* color="text.secondary" */ >
+                    Obten tus pokemons favoritos
+                    <br />{" "}
+                    <span className={styles.resaltado}>
+                      {" "}
+                      REACT NATIVE | EXPO CLI | STYLESHEET
+                    </span>
+                  </Typography>
+                </CardContent>
+              </CardActionArea >
+              <CardActions className={styles.bgBoton}>
+              <Link href="https://github.com/EnzoPrina/pokedex-r-native" legacyBehavior>
+                <Button className={styles.button} size="small" color="primary">
+                  Ver repositorio
+                </Button>
+                </Link>
+              </CardActions>
+            </Card>
+          </div>
+
         </div>
       </div>
     </>
